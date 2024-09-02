ed7cec1511b422472e7876451240e29ac33486c8
FraNLxrD <francisco.medrano.jy@gmail.com>
2024-09-02 18:29:54 -0300

    INICIO ESTRUCTURACION HTML Y CSS

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'src', 'views'))
+
+
+app.get('/', (req,res)=>{
+    res.render('index')
+})
+
+
+app.listen(3000, ()=>{
+    console.log("server en 3000")
+})
