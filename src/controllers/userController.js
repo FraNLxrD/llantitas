eea9b4f64d72300d5cbe4b7588dea26624d02fa6
FraNLxrD <francisco.medrano.jy@gmail.com>
2024-10-26 16:01:27 -0300

    MULTER COMPLETO USER Y FORM PRODUCTOS

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,8 @@
 // REQUIRE DATABASE
 const db = require("../database/models");
+const path = require("path");
+const fs = require("fs");
+const bcryptjs = require("bcryptjs");
 
 const userController= {
     login: (req,res) =>{
@@ -8,11 +11,18 @@ const userController= {
     registerView: (req,res) =>{
         res.render("user/register")
     },
-    register: (req, res) => {
+    register: async (req, res) => {
         console.log(req.body); // Para verificar qué datos se están recibiendo
         const { userName, email, password, image } = req.body;
-        const newUser = { userName, email, password, image };
-    
+        const fileUpload = req.file
+        const hasshedPassword = await bcryptjs.hash(password, 10)
+        console.log("Contrahasehada=", hasshedPassword)
+        
+
+
+        const newUser = { userName, email, password:hasshedPassword, image: fileUpload.filename };
+
+
         db.Users.create(newUser)
             .then(() => {
                 res.redirect("/user/login"); // Redirigir a la página de inicio de sesión
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const productRouter = express.Router();
-
+const { uploadProduct} = require("../config/multer.config")
 //CONTROLADOR
 const productController = require("../controllers/productController")
 
-productRouter.get("/form",productController.formget)
+productRouter.get("/sell",productController.formget)
+productRouter.post("/sell",uploadProduct.single("image"),productController.storeProduct)
 
 module.exports = productRouter;
